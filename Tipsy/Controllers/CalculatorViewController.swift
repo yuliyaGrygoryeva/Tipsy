1091824b2f18ae6f0664c29d4626f2651006136f
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-08-24 18:53:44 -0700

    First screen functionality added

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.1
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    var finalResult = "0.0"
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        let buttonTitle = sender.currentTitle!
+        let buttonNumber = Double(String(buttonTitle.dropLast()))
+        tip = buttonNumber! / 100
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+
+        
+    }
+    
+}
+
